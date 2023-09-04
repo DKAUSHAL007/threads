3a58902b81868c6cb01cd1c911788f0fcc29aa85
DISHANT <dishant1537.be20@chitkara.edu.in>
2023-09-04 20:19:24 +0530

    changes to leftsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,6 @@
 'use client'
-
 import {sidebarLinks} from '@/constants'
+import { SignOutButton, SignedIn } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import {usePathname,useRouter} from 'next/navigation'
@@ -15,7 +15,7 @@ function LeftSidebar(){
                     const isActive = (pathname.includes(link.route) && link.route.length>1)||pathname === link.route;
 
                     return(
-                        <Link href={link.route} key={link.label} className={`leftsidebar_link`${isActive && 'bg'}}>
+                        <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}>
                             <Image 
                                 src={link.imgURL}
                                 alt={link.label}
@@ -26,6 +26,16 @@ function LeftSidebar(){
                         </Link>
                     )}
                 )}
+            </div>
+            <div className="mt-10 px-6">
+                <SignedIn>
+                    <SignOutButton signOutCallback={()=>{router.push('/sign-in')}}>
+                        <div className="flex cursor-pointer">
+                            <Image src="/assets/logout.svg" alt='logout' width={24} height={24} />
+                            <p className="text-light-2 max-lg:hidden">Logout</p>
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
             </div>
         </section>
     )
